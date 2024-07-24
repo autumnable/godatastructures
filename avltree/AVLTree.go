e4c581769be6b267ca626ddd30111d2ab410c32c
Autumn Kelsey <autumnkelsey1@gmail.com>
2024-07-23 20:08:48 -0700

    done?

--- avltree/AVLTree.go
+++ avltree/AVLTree.go
@@ -3,10 +3,10 @@ package avltree
 import "golang.org/x/exp/constraints"
 
 type avlNode[K constraints.Ordered, V any] struct {
-	h           int
-	key         K
-	left, right *avlNode[K, V]
-	value       V
+	count_, height_ int
+	key             K
+	left, right     *avlNode[K, V]
+	value           V
 }
 
 func (this *avlNode[K, V]) balance() int {
@@ -14,6 +14,45 @@ func (this *avlNode[K, V]) balance() int {
 		return 0
 	}
 	return this.left.height() - this.right.height()
+}
+
+func (this *avlNode[K, V]) ceiling(key K) (K, V, bool) {
+	if this == nil {
+		return key, zeroValue[V](), false
+	} else if key > this.key {
+		return this.right.ceiling(key)
+	} else if key == this.key {
+		return key, this.value, true
+	} else if k, v, res := this.left.ceiling(key); res {
+		return k, v, res
+	} else {
+		return this.key, this.value, true
+	}
+}
+
+func (this *avlNode[K, V]) count() int {
+	if this == nil {
+		return 0
+	}
+	return this.count_
+}
+
+func (this *avlNode[K, V]) countGreater(key K) int {
+	if this == nil {
+		return 0
+	} else if key <= this.key {
+		return this.right.countGreater(key)
+	}
+	return 1 + this.right.count() + this.left.countGreater(key)
+}
+
+func (this *avlNode[K, V]) countLesser(key K) int {
+	if this == nil {
+		return 0
+	} else if key >= this.key {
+		return this.left.countGreater(key)
+	}
+	return 1 + this.left.count() + this.right.countLesser(key)
 }
 
 func (this *avlNode[K, V]) delete(key K) (res *avlNode[K, V], deleted *avlNode[K, V]) {
@@ -36,7 +75,8 @@ func (this *avlNode[K, V]) delete(key K) (res *avlNode[K, V], deleted *avlNode[K
 		this.right, _ = this.right.delete(deleted.key)
 	}
 
-	this.h = 1 + max(this.left.height(), this.right.height())
+	this.count_--
+	this.height_ = 1 + max(this.left.height(), this.right.height())
 	if b := this.balance(); b > 1 && this.left.balance() >= 0 {
 		return this.rightRotate(), deleted
 	} else if b > 1 {
@@ -49,6 +89,20 @@ func (this *avlNode[K, V]) delete(key K) (res *avlNode[K, V], deleted *avlNode[K
 		return this.leftRotate(), deleted
 	}
 	return this, deleted
+}
+
+func (this *avlNode[K, V]) floor(key K) (K, V, bool) {
+	if this == nil {
+		return key, zeroValue[V](), false
+	} else if key < this.key {
+		return this.left.floor(key)
+	} else if key == this.key {
+		return key, this.value, true
+	} else if k, v, res := this.right.floor(key); res {
+		return k, v, res
+	} else {
+		return this.key, this.value, true
+	}
 }
 
 func (this *avlNode[K, V]) get(key K) (V, bool) {
@@ -66,13 +120,13 @@ func (this *avlNode[K, V]) height() int {
 	if this == nil {
 		return 0
 	}
-	return this.h
+	return this.height_
 }
 
 func (this *avlNode[K, V]) insert(key K, value V) (*avlNode[K, V], bool) {
 	var res bool
 	if this == nil {
-		return &avlNode[K, V]{key: key, value: value}, true
+		return &avlNode[K, V]{count_: 1, key: key, value: value}, true
 	} else if key < this.key {
 		this.left, res = this.left.insert(key, value)
 	} else if key > this.key {
@@ -82,7 +136,8 @@ func (this *avlNode[K, V]) insert(key K, value V) (*avlNode[K, V], bool) {
 		return this, false
 	}
 
-	this.h = 1 + max(this.left.height(), this.right.height())
+	this.count_++
+	this.height_ = 1 + max(this.left.height(), this.right.height())
 
 	if b := this.balance(); b > 1 && key < this.left.key {
 		return this.rightRotate(), res
@@ -101,16 +156,24 @@ func (this *avlNode[K, V]) insert(key K, value V) (*avlNode[K, V], bool) {
 func (this *avlNode[K, V]) leftRotate() *avlNode[K, V] {
 	y := this.right
 	t2 := y.left
+
+	this.count_ = 1 + this.left.count() + t2.count()
+	y.count_ = 1 + this.count_ + y.right.count()
 	y.left, this.right = this, t2
 
-	this.h = max(this.left.h, this.right.h) + 1
-	y.h = max(y.left.h, y.right.h) + 1
+	this.height_ = max(this.left.height_, this.right.height_) + 1
+	y.height_ = max(y.left.height_, y.right.height_) + 1
 	return y
 }
 
+func (this *avlNode[K, V]) max() *avlNode[K, V] {
+	for ; this.right != nil; this = this.right {
+	}
+	return this
+}
+
 func (this *avlNode[K, V]) min() *avlNode[K, V] {
-	for this.left != nil {
-		this = this.left
+	for ; this.left != nil; this = this.left {
 	}
 	return this
 }
@@ -118,29 +181,60 @@ func (this *avlNode[K, V]) min() *avlNode[K, V] {
 func (this *avlNode[K, V]) rightRotate() *avlNode[K, V] {
 	x := this.left
 	t2 := x.right
+
+	this.count_ = 1 + this.right.count() + t2.count()
+	x.count_ = 1 + this.count_ + x.left.count()
 	x.right, this.left = this, t2
 
-	this.h = max(this.left.h, this.right.h) + 1
-	x.h = max(x.left.h, x.right.h) + 1
+	this.height_ = max(this.left.height_, this.right.height_) + 1
+	x.height_ = max(x.left.height_, x.right.height_) + 1
 	return x
 }
 
 type AVLTree[K constraints.Ordered, V any] struct {
-	count int
-	root  *avlNode[K, V]
+	root *avlNode[K, V]
 }
 
 func (this *AVLTree[K, V]) Add(key K, value V) bool {
 	var res bool
 	this.root, res = this.root.insert(key, value)
-	if res {
-		this.count++
-	}
 	return res
 }
 
+func (this *AVLTree[K, V]) Ceiling(key K) (K, V, bool) {
+	return this.root.ceiling(key)
+}
+
 func (this *AVLTree[K, V]) Count() int {
-	return this.count
+	return this.root.count()
+}
+
+func (this *AVLTree[K, V]) CountGreater(key K) int {
+	return this.root.countGreater(key)
+}
+
+func (this *AVLTree[K, V]) CountGreaterOrEqual(key K) int {
+	return this.root.count() - this.root.countLesser(key)
+}
+
+func (this *AVLTree[K, V]) CountLesser(key K) int {
+	return this.root.countLesser(key)
+}
+
+func (this *AVLTree[K, V]) CountLesserOrEqual(key K) int {
+	return this.root.count() - this.root.countGreater(key)
+}
+
+func (this *AVLTree[K, V]) First() (K, V, bool) {
+	first := this.root.min()
+	if first == nil {
+		return zeroValue[K](), zeroValue[V](), false
+	}
+	return first.key, first.value, true
+}
+
+func (this *AVLTree[K, V]) Floor(key K) (K, V, bool) {
+	return this.root.floor(key)
 }
 
 func (this *AVLTree[K, V]) Get(key K) (V, bool) {
@@ -156,11 +250,36 @@ func (this *AVLTree[K, V]) Height() int {
 	return this.root.height()
 }
 
+func (this *AVLTree[K, V]) Last() (K, V, bool) {
+	last := this.root.max()
+	if last == nil {
+		return zeroValue[K](), zeroValue[V](), false
+	}
+	return last.key, last.value, true
+}
+
+func (this *AVLTree[K, V]) PollFirst() (K, V, bool) {
+	k, v, has := this.First()
+	if !has {
+		return k, v, has
+	}
+	_, _ = this.Remove(k)
+	return k, v, true
+}
+
+func (this *AVLTree[K, V]) PollLast() (K, V, bool) {
+	k, v, has := this.Last()
+	if !has {
+		return k, v, has
+	}
+	_, _ = this.Remove(k)
+	return k, v, true
+}
+
 func (this *AVLTree[K, V]) Remove(key K) (V, bool) {
 	if _, res := this.root.delete(key); res == nil {
 		return zeroValue[V](), false
 	} else {
-		this.count--
 		return res.value, true
 	}
 }
